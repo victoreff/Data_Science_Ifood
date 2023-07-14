e2437200fb0b3e8c818cb470cb701401811690ee
victoreff <victorferreiraobdc@gmail.com>
2023-07-13 21:54:26 -0400

    Adicionando descrição ao arquivo <sistema_básico_bancário

--- Python - Curso Inicial/Desafio/sistema_básico_bancário.py
+++ Python - Curso Inicial/Desafio/sistema_básico_bancário.py
@@ -1,3 +1,22 @@
+""""
+Sistema Bancário Simples em Python
+
+Bem-vindo ao repositório do Sistema Bancário Simples em Python! Este projeto implementa um sistema básico de gerenciamento de conta bancária, permitindo depósitos, saques e a visualização do extrato.
+
+O sistema utiliza a linguagem de programação Python e oferece uma interface de linha de comando simples e intuitiva. Ao executar o programa, o usuário é apresentado a um menu com as opções de depósito, saque, extrato e sair.
+
+Durante a execução, o usuário pode realizar depósitos informando o valor desejado, verificar o saldo disponível, efetuar saques respeitando um limite de operações e consultar o extrato contendo todas as movimentações realizadas.
+
+O projeto utiliza variáveis para rastrear o saldo, o número de saques, o limite de operações e o extrato. O código é estruturado em um loop contínuo, permitindo que o usuário execute múltiplas operações antes de decidir sair do sistema.
+
+Este repositório serve como um exemplo básico de aplicação de conceitos de programação em Python para criar um sistema bancário simples. É útil para fins educacionais e para quem deseja entender os fundamentos de um sistema de conta bancária.
+
+Fique à vontade para explorar o código-fonte, fazer melhorias ou adaptá-lo às suas necessidades. Contribuições são bem-vindas!
+
+Divirta-se usando o Sistema Bancário Simples em Python!
+"""
+
+
 extrato = ""
 saldo = 0
 valor_limite_de_saque = 500
